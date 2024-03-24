4340a7b53e935c76ff5f0513922ff07cab5f3c33
cihuyyama <miqbalalhabib@gmail.com>
2024-03-25 01:04:19 +0700

    feat: add active path

--- components/SideBarList.tsx
+++ components/SideBarList.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Sidebar, { SidebarItem } from './SideBar'
 import { BarChartIcon, FileClock, FlaskConical, HomeIcon, UserIcon } from 'lucide-react'
+import { usePathname } from 'next/navigation'
 
 function SideBarList() {
+    const pathname = usePathname()
+
     return (
         <Sidebar>
-            <SidebarItem icon={<HomeIcon />} link="/diseases" text="Disease" />
-            <SidebarItem icon={<BarChartIcon />} link="/endemics" text="Endemicity" />
-            <SidebarItem icon={<FlaskConical />} link="/symptoms" text="Symptom" />
-            <SidebarItem icon={<FileClock />} link="/logs" text="Log" />
-            <SidebarItem icon={<UserIcon />} link="#" text="Users" />
+            <SidebarItem active={pathname==='/diseases'} icon={<HomeIcon />} link="/diseases" text="Disease" />
+            <SidebarItem active={pathname==='/endemics'} icon={<BarChartIcon />} link="/endemics" text="Endemicity" />
+            <SidebarItem active={pathname==='/symptoms'} icon={<FlaskConical />} link="/symptoms" text="Symptom" />
+            <SidebarItem active={pathname==='/logs'} icon={<FileClock />} link="/logs" text="Log" />
+            <SidebarItem active={pathname==='/users'} icon={<UserIcon />} link="#" text="Users" />
         </Sidebar>
     )
 }
